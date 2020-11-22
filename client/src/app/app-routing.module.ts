a15a1c3ac110ebc0be5bda22bc360f953a66f5ef
AsiaC <czaplicka.asia@gmail.com>
2020-11-22 01:12:14 +0100

    client: guard-ograniczenie przed wyborem linka

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 const routes: Routes = [
 {path:'', component: HomeComponent},
-{path:'recipes', component: RecipeListComponent},
+{path:'recipes', component: RecipeListComponent, canActivate: [AuthGuard]},
 {path:'recipes/:id', component: RecipeDetailComponent},
 {path:'**', component: HomeComponent, pathMatch:'full'}
 ];

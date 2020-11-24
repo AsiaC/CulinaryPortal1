8c7d59a6caf3ed475711a4c8e615781ba0b8c02c
AsiaC <czaplicka.asia@gmail.com>
2020-11-24 17:38:07 +0100

    Api: mapper-stworzenie profili

---
+++ CulinaryPortal.API/Profiles/CookbookProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulinaryPortal.API.Profiles
+{
+    public class CookbookProfile: Profile
+    {
+        public CookbookProfile()
+        {
+            CreateMap<Entities.Cookbook, Models.CookbookDto>();
+        }
+    }
+}
---
+++ CulinaryPortal.API/Profiles/IngredientProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CulinaryPortal.API.Profiles
+{
+    public class IngredientProfile: Profile
+    {
+        public IngredientProfile()
+        {
+            CreateMap<Entities.Ingredient, Models.IngredientDto>();
+        }
+    }
+}
